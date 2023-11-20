0de6c38aeb260b594d9380ac6cfad485c54438f7
Chris Moscoso <Christophermoscoso@gmail.com>
2023-11-20 11:14:58 -0500

    Modify Command to be type generic with the Result

--- src/Command/Command.ts
+++ src/Command/Command.ts
@@ -5,11 +5,14 @@ import { Result } from '../Result/Result';
  * Command objects allow for loosely coupled systems
  * by separating the objects that issue a request
  * from the objects that actually process the request.
+ *
+ * A command can execute to return a Result
+ * @type R - the type of Result returned after executing
  */
-export interface Command {
+export interface Command <R extends Result<any, any>> {
     /**
      * The function to trigger when this command is invoked.
      * It returns a {@link Result} which provides context as to whether the command succeeded or failed to execute.
      */
-    execute(): Result < any, any > ;
+    execute(): R;
 }
