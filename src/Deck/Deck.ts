b0cff262f822c6553f0749cbb247f0e4b08cceda
Chris Moscoso <Christophermoscoso@gmail.com>
2023-11-21 00:04:55 -0500

    Created Deck module

---
+++ src/Deck/Deck.ts
@@ -0,0 +1,42 @@
+import { shuffle } from '../Utility';
+
+export class Deck<T> {
+	protected cardList: T[] = [];
+
+	constructor() {
+
+	}
+
+	shuffle (): T[] {
+		this.cardList = shuffle(this.cardList);
+		return this.cards;
+	}
+
+	static shuffleCards<C>(cards: C[]): C[] {
+		return shuffle(cards);
+	}
+
+	get cards (): T[] {
+		return this.cardList;
+	}
+
+	get code(): string {
+        let code = '';
+        for (const card of this.cards) {
+            code += card?.toString() ?? JSON.stringify(card);
+        }
+        return code;
+    }
+
+	get hasCards(): boolean {
+        return this.cards.length > 0;
+    }
+
+    get isEmpty(): boolean {
+        return this.cards.length === 0;
+    }
+
+	get size (): number {
+		return this.cards.length;
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './Aggregate/Aggregate';
 export * from './AppError/AppError';
 export * from './AppEvent/AppEvent';
 export * from './Command/Command';
+export * from './Deck/Deck';
 export * from './Precondition/Precondition';
 export * from './Queue/Queue';
 export * from './Reducer/CombineReducer';
