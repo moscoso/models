e2da622d0d574077ae8db95ef0efd736b2d4bb38
Chris Moscoso <Christophermoscoso@gmail.com>
2024-03-03 02:47:03 -0500

    Improve jsdocs for ObjectUtility's modifyNumber method

--- src/Utility/Object.ts
+++ src/Utility/Object.ts
@@ -2,15 +2,20 @@ import { deepCopy } from './DeepCopy';
 
 export class ObjectUtility {
 	/**
-	 * Modifies a numeric property of objects within a given map. This function can access and modify
-	 * both direct and nested numeric properties based on the provided property path. If the property
-	 * at the specified path is not a number, or if the object does not exist, the function throws an error.
+	 * Creates and returns a copy of the given map of objects, with specified numeric properties modified
+	 * according to the given change and constraints. This function can access and modify both direct and nested
+	 * numeric properties based on the provided property path. It ensures the original objects are not altered.
+	 *
+	 * If the property at the specified path is not a number, or if the object does not exist, the function throws an error.
 	 *
 	 * @template T - The type of the objects within the map.
 	 * @param {Record<string, T>} objects - A map of objects keyed by string identifiers.
 	 * @param {string | string[]} objectIDs - An identifier or array of identifiers for the objects to be modified.
 	 * @param {string} propertyPath - The path to the numeric property to be modified, which can include nested properties separated by dots (e.g., 'property.nestedProperty').
 	 * @param {number} change - The amount by which to modify the property's value. Can be positive (for incrementing) or negative (for decrementing).
+	 * @param {Object} constraints - Optional constraints to apply to the modification.
+	 * @param {number} [constraints.min=0] - The minimum value the property can have after modification. Defaults to 0.
+	 * @param {number} [constraints.max=Number.MAX_VALUE] - The maximum value the property can have after modification. Defaults to the maximum safe integer value.
 	 * @returns {Record<string, T>} - a copy of the object with the modified properties.
 	 * @throws {Error} - Throws an error if an object identifier does not exist in the map, if the property path is invalid, or if the specified property is not a number.
 	 */
