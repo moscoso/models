1fa27e80e986fd5c8785a8befd6d96c6b2634769
Chris Moscoso <Christophermoscoso@gmail.com>
2024-09-18 14:00:00 +0000

    Refactor Command to include 'name' + 'params' property
    Now Commands will have an extra type param P for its 'params' property

--- src/Command/Command.ts
+++ src/Command/Command.ts
@@ -9,7 +9,12 @@ import { Result } from '../Result/Result';
  * A command can execute to return a Result
  * @type R - the type of Result returned after executing
  */
-export interface Command <R extends Result<any, any, any>> {
+export interface Command <R extends Result<any, any, any>, P = {}> {
+	/** The name of the command */
+	name: string;
+	/** The parameters required to execute the command. */
+	params: P;
+
     /**
      * The function to trigger when this command is invoked.
      * It returns a {@link Result} which provides context as to whether the command succeeded or failed to execute.
