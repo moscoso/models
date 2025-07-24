82d4519644e7aa1bfd5ef3d7e188cd87416d8589
Chris Moscoso <Christophermoscoso@gmail.com>
2025-07-24 08:55:21 -0400

    Delete `Precondition` utility class
    Use the exported functions instead

--- src/Precondition/Precondition.ts
+++ src/Precondition/Precondition.ts
@@ -17,43 +17,6 @@ export type PreconditionValidator<E extends Error> = (args: any) => E | null;
 export type ConfigurablePreconditionValidator<P extends PreconditionValidator<any>> = (...params: any[]) => P;
 
 /**
- * A utility class to help validate preconditions for a Command
- * @deprecated Please use the functions instead of this utility class
- */
-export class Precondition {
-	/**
-	 * Validates that a list of preconditions are met
-	 * @param preconditions an array of preconditions
-	 * @param args an object with the arguments of a command
-	 * @returns `null` if all preconditions are met, or an {@link Error} specifying why a precondition failed
-	 */
-	public static validate<E extends Error>(preconditions: PreconditionValidator<E>[], args: any): E | null {
-		for (const precondition of preconditions) {
-			const error = precondition(args);
-			if (error) {
-				return error;
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Validates that all the args are defined
-	 * @param args an object with presumably all the properties specified by `argNames`
-	 * @param argNames the list of argument names to check for
-	 * @returns an {@link AppError} if `args` does not have a property with key of `argName`
-	 */
-	public static validateArgsAreDefined(args: any, argNames: string[]): AppError<string> | undefined {
-		const missingArgs = argNames.filter(argName => args[argName] == null);
-		if (missingArgs.length) {
-			const error = new AppError(`Undefined arguments were passed into the command: ${missingArgs}`);
-			error.setDetail(`${missingArgs} was undefined`);
-			return error;
-		}
-	}
-}
-
-/**
  * Validates that a list of preconditions are met
  *
  * @param preconditions an array of preconditions
