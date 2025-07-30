e3759362bbf0ccdbe87b5225784869e04fa1e5c3
Chris Moscoso <christophermoscoso@gmail.com>
2025-07-30 13:24:40 -0400

    Refactor `Aggregate` + `Command` modules
    Merge `CommandDrivenAggregate` into `Aggregate` because it was only one method. Move the `CommandResult` type into it's own file

--- src/Command/Command.ts
+++ src/Command/Command.ts
@@ -1,3 +1,4 @@
+import { AppEvent } from '../AppEvent/AppEvent';
 import { Result } from '../Result/Result';
 
 /**
@@ -6,8 +7,7 @@ import { Result } from '../Result/Result';
  * by separating the objects that issue a request
  * from the objects that actually process the request.
  *
- * A command can execute to return a Result
- * @type R - the type of Result returned after executing
+ * @type R - the type of {@link Result} returned after executing
  */
 export interface Command <R extends Result<any, any, any>, P = {}> {
 	/** The name of the command */
@@ -17,7 +17,33 @@ export interface Command <R extends Result<any, any, any>, P = {}> {
 
     /**
      * The function to trigger when this command is invoked.
-     * It returns a {@link Result} which provides context as to whether the command succeeded or failed to execute.
+     * It returns a {@link Result} which specifies whether the execution succeeded or failed.
      */
     execute(): R;
 }
+
+/**
+ * The value returned from a successful {@link CommandResult}.
+ *
+ * Contains the array of {@link AppEvent}s produced as well as
+ * any follow-up {@link Command}s that should be executed afterwards.
+ */
+export type CommandExecution<
+	E extends AppEvent<string, object>,
+	C extends Command<Result<object, any, object>, any>
+> = {
+	events: E[];
+	commands: C[];
+};
+
+/**
+ * A {@link CommandResult} represents the {@link Result} of executing a {@link Command}.
+ *
+ * – On success, it returns a {@link CommandExecution} containing events and follow-up commands.
+ *
+ * – On failure, it returns any error
+ */
+export type CommandResult<
+	E extends AppEvent<string, object>,
+	C extends Command<Result<object, any, object>, any>
+> = Result<CommandExecution<E, C>, any, CommandExecution<E, C>>
